101d6451af8b4bb856ff4b29fbdd27c33c520515
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-08-28 16:43:47 +0100

    update

--- src/components/Pages/Crew.tsx
+++ src/components/Pages/Crew.tsx
@@ -1,8 +1,27 @@
 import { Layout } from "../Layout"
-import {AnimatePresence, motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import '../../crew.css'
+import data from '../../data.json'
+import { useState } from "react"
 
-export const Crew:React.FC = () => {
+export const Crew: React.FC = () => {
+  const [selectedCrewMember, setSelectedCrewMember] = useState<
+  Crew
+>(data.crew[0]);
+const [selectedDot, setSelectedDot] = useState(data.crew[0].name);
+const handleDotClick = (dot:Crew) => {
+  setSelectedCrewMember(dot);
+  setSelectedDot(dot.name)
+};
+interface Crew {
+  name: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  role: string;
+  bio: string;
+}
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { duration: 0.75 } },
@@ -13,32 +32,51 @@ export const Crew:React.FC = () => {
   //   hidden: { y: -20, opacity: 0 },
   //   visible: { y: 0, opacity: 1, transition: { duration: 0.75, delay: 0.2 } }
   // };
-
-  const imageVariants = {
-    hidden: { scale: 0 },
-    visible: { scale: 1, transition: { duration: 0.5, delay: 0.5 } }
-  };
+  // const imageVariants = {
+  //   hidden: { scale: 0 },
+  //   visible: { scale: 1, transition: { duration: 0.5, delay: 0.5 } }
+  // };
   return (
-    <motion.div 
-    className="crew"
-    variants={containerVariants}
-    initial="hidden"
-    animate="visible"
-    exit="exit"
+    <motion.div
+      className="crew"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-    <Layout>
-      <div className="title">
-        <h1><span>02</span>MEET YOUR CREW</h1>
-      </div>
-      <div className="content">
-        <AnimatePresence mode="popLayout">
-          <motion.div className="text-content"></motion.div>
-          <motion.div className="commander-div">
-            <motion.img src="" alt="" variants={imageVariants} />
-          </motion.div>
-        </AnimatePresence>
-      </div>
-    </Layout>
-  </motion.div>
+      <Layout>
+        <div className="title">
+          <h1><span>02</span>MEET YOUR CREW</h1>
+        </div>
+        <div className="content">
+            <div className="text-content">
+              <div className="info">
+                <div className="role">
+                  <h3>{selectedCrewMember.role}</h3>
+                </div>
+                <div className="name">
+                  <h2>{selectedCrewMember.name}</h2>
+                </div>
+                <div className="bio">
+                <p>{selectedCrewMember.bio}</p>
+                </div>
+              </div>
+              <nav className="dot-nav">
+                {data.crew.map(crewMember => (
+                  <div 
+                  className={`dot ${crewMember.name === selectedDot ? 'active' : ''}`}
+                  key={crewMember.name}
+                  onClick={() => handleDotClick(crewMember)}>
+
+                  </div>
+                ))}
+              </nav>
+            </div>
+            <div className="image-div">
+              <img src={selectedCrewMember.images.png} alt={selectedCrewMember.name}/>
+            </div>
+        </div>
+      </Layout>
+    </motion.div>
   )
 }
--- src/components/Pages/Destination.tsx
+++ src/components/Pages/Destination.tsx
@@ -1,16 +1,19 @@
 import { Layout } from "../Layout"
-import {motion, AnimatePresence} from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import data from '../../data.json'
 import '../../destination.css'
 import { useState } from "react"
 
 
-export const Destination:React.FC = () => {
+export const Destination: React.FC = () => {
   const [selectedDestination, setSelectedDestination] = useState<
     Destination
   >(data.destinations[0]);
-  const handleNavClick = (destination:Destination) => {
+  const [selectedLink, setSelectedLink] = useState(data.destinations[0].name);
+
+  const handleNavClick = (destination: Destination) => {
     setSelectedDestination(destination);
+    setSelectedLink(destination.name);
   };
   interface Destination {
     name: string;
@@ -22,7 +25,7 @@ export const Destination:React.FC = () => {
     distance: string;
     travel: string;
   }
-  
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { duration: 0.75 } },
@@ -39,58 +42,61 @@ export const Destination:React.FC = () => {
     visible: { scale: 1, transition: { duration: 0.5, delay: 0.5 } }
   };
   return (
-    <motion.div 
-    className="destination"
-    variants={containerVariants}
-    initial="hidden"
-    animate="visible"
-    exit="exit"
+    <motion.div
+      className="destination"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-    <Layout>
-      <div className="title">
-        <h1><span>01</span>PICK YOUR DESTINATION</h1>
-      </div>
-      <div className="content">
-        <AnimatePresence mode="popLayout">
-        <div className="planet-div">
-          <motion.img src={selectedDestination.images.png} alt={selectedDestination.name} variants={imageVariants} />
+      <Layout>
+        <div className="title">
+          <h1><span>01</span>PICK YOUR DESTINATION</h1>
+        </div>
+        <AnimatePresence>
+        <div className="content">
+          <AnimatePresence mode="popLayout">
+            <div className="planet-div">
+              <motion.img src={selectedDestination.images.png} alt={selectedDestination.name} variants={imageVariants} />
+            </div>
+          </AnimatePresence>
+          <AnimatePresence>
+            <motion.div className="text-content" variants={textVariants}>
+              <nav className="nav">
+                {data.destinations.map(destination => (
+                  <p
+                    key={destination.name}
+                    className={`link ${destination.name === selectedLink ? "active" : ""}`}
+
+                    onClick={() => handleNavClick(destination)}
+                  >
+                    {destination.name}
+                  </p>
+                ))}
+              </nav>
+              <div className="description">
+                <motion.div className="name" variants={textVariants}>
+                  <h2>{selectedDestination.name}</h2>
+                </motion.div>
+                <motion.div className="intro" variants={textVariants}>
+                  <p>{selectedDestination.description}</p>
+                </motion.div>
+              </div>
+              <div className="statistics">
+                <div className="distance">
+                  <h3>AVG. DISTANCE</h3>
+                  <p>{selectedDestination.distance}</p>
+                </div>
+                <div className="time">
+                  <h3>Est. travel time</h3>
+                  <p>{selectedDestination.travel}</p>
+                </div>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
         </AnimatePresence>
-        <AnimatePresence>
-        <motion.div className="text-content" variants={textVariants}>
-          <nav className="nav">
-          {data.destinations.map(destination => (
-            <p
-              key={destination.name}
-              className={`link ${destination.name} ${onclick=() =>{'active'}}`}
-              onClick={() => handleNavClick(destination)}
-            >
-              {destination.name}
-            </p>
-          ))}
-          </nav>
-          <div className="description">
-            <div className="name">
-              <h2>{selectedDestination.name}</h2>
-            </div>
-            <div className="intro">
-              <p>{selectedDestination.description}</p>
-            </div>
-          </div>
-          <div className="statistics">
-            <div className="distance">
-              <h3>AVG. DISTANCE</h3>
-              <p>{selectedDestination.distance}</p>
-            </div>
-            <div className="time">
-              <h3>Est. travel time</h3>
-              <p>{selectedDestination.travel}</p>
-            </div>
-          </div>
-        </motion.div>
-        </AnimatePresence>
-      </div>
-    </Layout>
-  </motion.div>
+      </Layout>
+    </motion.div>
   )
 }
